5a5769ecbb2638a2439747982b3e6794ee0ee12b
Tian Jie Wong <wtj.wongtianjie@gmail.com>
2023-05-23 23:15:18 +0100

    Update models. Register models with Django admin and configure list_display.
    Update models.py, admins.py.

--- apps/web/portfolio/admin.py
+++ apps/web/portfolio/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
+from .models import *
+
 # Register your models here.
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'start_date', 'end_date')
+
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category')
+
+@admin.register(Experience)
+class ExperienceAdmin(admin.ModelAdmin):
+    list_display = ('title', 'institution','start_date', 'end_date')
+
+@admin.register(Education)
+class EducationAdmin(admin.ModelAdmin):
+    list_display = ('title','institution','start_date', 'end_date')
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'semester')
+
+@admin.register(Semester)
+class SemesterAdmin(admin.ModelAdmin):
+    list_display = ('number','start_date', 'end_date')
+
+@admin.register(ContactForm)
+class ContactFormAdmin(admin.ModelAdmin):
+    list_display = ('subject', 'name', 'email', 'created_at')
--- apps/web/portfolio/models.py
+++ apps/web/portfolio/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.text import slugify
 from cloudinary.models import CloudinaryField
+from datetime import date
 
 # Create your models here.
 class Project(models.Model):
@@ -8,7 +9,8 @@ class Project(models.Model):
     description = models.TextField()
     slug = models.SlugField(max_length=255, unique=True)
     start_date = models.DateField()
-    end_date = models.DateField()
+    end_date = models.DateField(null=True, default=None)
+    current = models.BooleanField(default=False)
     link_github = models.URLField(blank=True)
     link_project = models.URLField(blank=True)
     logo = CloudinaryField("Image", overwrite=True, format="jpg", blank=True)
@@ -27,8 +29,15 @@ class Project(models.Model):
 
         if Project.objects.filter(slug=to_assign).exists():
             to_assign += f"-{str(Project.objects.filter(slug=to_assign).count())}"
+        self.slug = to_assign   
+
+        if self.start_date < self.end_date:
+            raise ValueError("Start date must be before end date!")
+        if self.end_date is None or self.end_date > date.today():
+            self.current = True
+        else:
+            self.current = False
         
-        self.slug = to_assign
         super().save(*args, **kwargs)
 
 class Skill(models.Model):
@@ -49,36 +58,62 @@ class Experience(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     start_date = models.DateField()
-    end_date = models.DateField()
+    end_date = models.DateField(null=True, default=None)
+    current = models.BooleanField(default=False)
     location = models.CharField(max_length=200)
     institution = models.CharField(max_length=200)
     logo = CloudinaryField("Image", overwrite=True, format="jpg", blank=True)
     link = models.URLField(blank=True)
     skills = models.ManyToManyField(Skill)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ['-end_date', '-start_date']
 
     def __str__(self):
         return f"{self.title} at {self.institution}"
+    
+    def save(self, *args, **kwargs):
+        if self.start_date < self.end_date:
+            raise ValueError("Start date must be before end date!")
+        if self.end_date is None or self.end_date > date.today():
+            self.current = True
+        else:
+            self.current = False
+        
+        super().save(*args, **kwargs)
 
 class Education(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     start_date = models.DateField()
-    end_date = models.DateField()
+    end_date = models.DateField(null=True, default=None)
+    current = models.BooleanField(default=False)
     location = models.CharField(max_length=200)
     institution = models.CharField(max_length=200)
     logo = CloudinaryField("Image", overwrite=True, format="jpg", blank=True)
     link = models.URLField(blank=True)
     grade = models.CharField(max_length=200)
     subjects = models.ManyToManyField('Subject')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ['-end_date', '-start_date']
 
     def __str__(self):
         return f"{self.title} at {self.institution}"
+    
+    def save(self, *args, **kwargs):
+        if self.start_date < self.end_date:
+            raise ValueError("Start date must be before end date!")
+        if self.end_date is None or self.end_date > date.today():
+            self.current = True
+        else:
+            self.current = False
+        
+        super().save(*args, **kwargs)
 
 class Subject(models.Model):
     name = models.CharField(max_length=200)
