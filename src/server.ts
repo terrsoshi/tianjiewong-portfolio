787d9942bd31a974da876c308273387f42bb0a1a
Tian Jie Wong <wtj.wongtianjie@gmail.com>
2024-05-20 06:21:50 +0100

    Add connection to database and Modify .env.sample accordingly.

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,26 @@
 import cors from "cors";
 import dotenv from "dotenv";
+import { MongoClient, Db } from "mongodb";
 import express, { Express, Request, Response } from "express";
 
+// Load and use environment variables from the .env file
 dotenv.config();
-const { SERVER_PORT = 8000 } = process.env;
+const { SERVER_PORT = 8000, DB_URI = "", DB_NAME = "" } = process.env;
+
+// Connect to MongoDB Database
+const mongoClient: MongoClient = new MongoClient(DB_URI);
+let db: Db;
+const connectDB = async (): Promise<void> => {
+  console.log(`Connecting to the ${DB_NAME} database...`);
+  try {
+    await mongoClient.connect();
+    db = mongoClient.db(DB_NAME);
+    console.log(`Successfully connected to the ${DB_NAME} database!`);
+  } catch (err) {
+    console.error(`Connection to the ${DB_NAME} database failed!`, err);
+  }
+};
+connectDB();
 
 const app: Express = express();
 app.use(cors());
