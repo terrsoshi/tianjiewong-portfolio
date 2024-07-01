0d19ca16e049996690aa0cfacaf99c8160cb1011
Tian Jie Wong <wtj.wongtianjie@gmail.com>
2024-07-01 05:55:20 +0100

    Add Header component with logo, navigation bar, and Dark mode switch.

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -18,10 +18,11 @@ import {
 
 import ThemeToggleButton from "./ThemeToggleButton";
 
+const menuItems = ["Home", "About Me", "Skills", "Projects", "Contact Me"];
+const menuItemsId = ["", "about", "skills", "projects", "about"];
+
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  const menuItems = ["Home", "About Me", "Skills", "Projects", "Contact Me"];
 
   return (
     <header>
@@ -32,48 +33,57 @@ const Header = () => {
             aria-label={isMenuOpen ? "Close menu" : "Open menu"}
           />
           <NavbarBrand>
-            <Link className="flex gap-1" href="/">
+            <Link
+              className="flex gap-1 text-current hover:opacity-100"
+              href="/"
+            >
               <Image
                 src="/logo.png"
                 alt="Tian Jie Wong portfolio site logo"
                 width="24"
                 height="24"
               />
-              <p>Tian Jie Wong</p>
+              <p className="font-semibold text-black antialiased dark:text-white">
+                Tian Jie Wong
+              </p>
             </Link>
           </NavbarBrand>
         </NavbarContent>
 
-        <NavbarContent className="hidden gap-4 lg:flex" justify="center">
-          <NavbarItem>
-            <Link href="#">Home</Link>
-          </NavbarItem>
-          <NavbarItem>
-            <Link href="#">About Me</Link>
-          </NavbarItem>
-          <NavbarItem>
-            <Link href="#">Skills</Link>
-          </NavbarItem>
-          <NavbarItem>
-            <Link href="#">Projects</Link>
-          </NavbarItem>
-          <NavbarItem>
-            <Link href="#">Contact Me</Link>
-          </NavbarItem>
+        <NavbarContent className="hidden gap-8 lg:flex" justify="center">
+          {menuItems.map((item, index) => {
+            return (
+              <NavbarItem key={`${item}-${index}`}>
+                <Link
+                  className="text-current antialiased hover:opacity-50 active:underline active:underline-offset-4 dark:hover:text-white dark:hover:opacity-100"
+                  href={`/#${menuItemsId[index]}`}
+                  size="lg"
+                >
+                  {item}
+                </Link>
+              </NavbarItem>
+            );
+          })}
         </NavbarContent>
 
         <NavbarContent justify="end">
           <ThemeToggleButton />
         </NavbarContent>
 
-        <NavbarMenu className="flex gap-0">
-          {menuItems.map((item, index) => (
-            <NavbarMenuItem key={`${item}-${index}`}>
-              <Link className="w-full" href="#">
-                {item}
-              </Link>
-            </NavbarMenuItem>
-          ))}
+        <NavbarMenu className="flex gap-2 dark:bg-black/50">
+          {menuItems.map((item, index) => {
+            return (
+              <NavbarMenuItem key={`menu-${item}-${index}`}>
+                <Link
+                  className="w-full rounded-xl px-4 py-3 text-current antialiased hover:bg-zinc-200 hover:text-black hover:opacity-100 dark:hover:bg-zinc-700/70 dark:hover:text-white"
+                  href={`/#${menuItemsId[index]}`}
+                  size="lg"
+                >
+                  {item}
+                </Link>
+              </NavbarMenuItem>
+            );
+          })}
         </NavbarMenu>
       </Navbar>
 
